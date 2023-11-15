50cc888d30386f98791d4dccde763dd1dde7ef23
Elias Corrêa <eliascorreajr@gmail.com>
2023-11-15 13:55:04 -0300

    Atualização para Primeira Entrega do Projeto

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-// Definindo o objeto SimuladorParcelamento
-function SimuladorParcelamento(valorTotal) {
-  this.valorTotal = valorTotal;
+function SimuladorParcelamento() {
+  this.valorTotal = 0;
   this.quantidadeParcelas = 0;
+  this.taxaJuros = 0;
+  this.periodoEmMeses = 0;
 }
 
-// Método para solicitar a quantidade de parcelas ao usuário
+SimuladorParcelamento.prototype.solicitarValorTotal = function () {
+  while (true) {
+    this.valorTotal = parseFloat(prompt("Digite o valor total a ser financiado (use ponto para separar os milhares e vírgula para os decimais, se necessário). Exemplo: 10000,50 para dez mil reais e cinquenta centavos:"));
+    if (!isNaN(this.valorTotal) && this.valorTotal > 0) {
+      break;
+    } else {
+      alert("Valor inválido. Por favor, digite um número válido maior que zero.");
+    }
+  }
+};
+
 SimuladorParcelamento.prototype.solicitarQuantidadeParcelas = function () {
   while (true) {
-    this.quantidadeParcelas = parseInt(prompt("Digite a quantidade de parcelas desejada:"));
-
+    this.quantidadeParcelas = parseInt(prompt("Digite a quantidade de parcelas desejada (apenas números inteiros). Exemplo: 12 para doze parcelas:"));
     if (!isNaN(this.quantidadeParcelas) && this.quantidadeParcelas > 0) {
       break;
     } else {
@@ -17,25 +27,32 @@ SimuladorParcelamento.prototype.solicitarQuantidadeParcelas = function () {
   }
 };
 
-// Método para calcular e exibir as parcelas
+SimuladorParcelamento.prototype.solicitarTaxaJuros = function () {
+  while (true) {
+    this.taxaJuros = parseFloat(prompt("Digite a taxa de juros mensal (em %). Use ponto para decimais. Exemplo: 1.5 para uma taxa de 1,5%:"));
+    if (!isNaN(this.taxaJuros) && this.taxaJuros > 0) {
+      break;
+    } else {
+      alert("Taxa de juros inválida. Por favor, digite um número válido maior que zero.");
+    }
+  }
+};
+
 SimuladorParcelamento.prototype.calcularParcelamento = function () {
-  const valorParcela = this.valorTotal / this.quantidadeParcelas;
+  const taxaMensal = this.taxaJuros / 100;
+  let valorParcela = this.valorTotal / this.quantidadeParcelas;
 
   alert("Simulação de parcelamento:");
   for (let i = 1; i <= this.quantidadeParcelas; i++) {
+    valorParcela += valorParcela * taxaMensal;
     alert(`Parcela ${i}: R$ ${valorParcela.toFixed(2)}`);
   }
 };
 
-// Criando uma instância do SimuladorParcelamento com um valor total de R$ 100.000
-const simulador = new SimuladorParcelamento(100000);
+const simulador = new SimuladorParcelamento();
+alert("Bem-vindo ao simulador de parcelamento de financiamento.");
 
-alert("Bem-vindo ao simulador de parcelamento em 5 anos.");
-alert(`Você tem um valor total de R$ ${simulador.valorTotal.toFixed(2)}`);
-
-// Solicitando a quantidade de parcelas ao usuário usando o método do objeto
+simulador.solicitarValorTotal();
 simulador.solicitarQuantidadeParcelas();
-
-// Calculando e exibindo as parcelas usando o método do objeto
+simulador.solicitarTaxaJuros();
 simulador.calcularParcelamento();
-
